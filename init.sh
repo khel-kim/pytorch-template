3852c7ca04fc724af95063b0baadd4718804095a
Khel Kim <cafehero123@gmail.com>
2020-08-26 13:51:57 +0900

    jupyter kernel install

--- init.sh
+++ init.sh
@@ -2,8 +2,7 @@
 mkdir data models config custom
 virtualenv env
 
-rm -rf .git
-
 source env/bin/activate
 pip3 install -r requirements.txt
 python3 -m ipykernel install --user --name=env --display-name=$PWD
+jupyter nbextension enable --py widgetsnbextension
